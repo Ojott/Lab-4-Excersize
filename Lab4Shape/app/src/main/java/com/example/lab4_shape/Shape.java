e962efab5cf581cf4681dcf3a707e780bc501663
shrpate <44587575+shrpate@users.noreply.github.com>
2020-02-07 13:56:27 -0700

    Update Shape.java

--- Lab4Shape/app/src/main/java/com/example/lab4_shape/Shape.java
+++ Lab4Shape/app/src/main/java/com/example/lab4_shape/Shape.java
@@ -3,6 +3,8 @@ package com.example.lab4_shape;
 public abstract class Shape {
     private int x;
     private int y;
+    
+    String color = "Green";
 
     public Shape (int x, int y) {
         this.x = x;
