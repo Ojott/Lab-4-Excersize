0494cdcdb5858949aff77b2f6cf643873277cc33
Akhi Patel <ak2@ualberta.ca>
2020-02-07 00:41:51 -0700

    new color RED

--- Lab4Shape/app/src/main/java/com/example/lab4_shape/Shape.java
+++ Lab4Shape/app/src/main/java/com/example/lab4_shape/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
     
-    String color = "Burgundy";
+    String color = "RED";
 
     public Shape (int x, int y) {
         this.x = x;
